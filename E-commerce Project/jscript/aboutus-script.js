61a81c5c2ced6f0456a6e720ff6117095fcdd9ae
Amr Sayed Ahmad Shaheen <engamroshaheen@gmail.com>
2020-05-19 16:38:36 +0200

    category and about us pages editing
    Complete category and about us pages

---
+++ E-commerce Project/jscript/aboutus-script.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+ window.open('mailto:mazen@example.com');
+}
+
+function myFunction2() {
+ window.open('mailto:amr@example.com');
+}
+
+function myFunction3() {
+ window.open('mailto:mohamed@example.com');
+}
+
+function myFunction4() {
+ window.open('mailto:ahmed@example.com');
+}
